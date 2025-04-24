fa4fc4d2eed8deeb49357f853d105ba28f86913e
ArnabhS <sinhaarnabh888@gmail.com>
2025-04-24 13:52:38 +0530

    fix: CORS error

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,20 @@ dotenv.config({ path: "./.env" });
 const app = express();
 const port =  process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+    origin: [ process.env.CORS_ORIGIN || "*" ], 
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+    "Origin",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "X-Request-With",
+    ],
+    })
+    );
 app.use(bodyParser.json());
 
 app.use("/api/auth/",authRoutes)
